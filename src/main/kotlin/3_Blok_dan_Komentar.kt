60cc802c87581bbe90ce996b49c7887c776e4058
RPL-YuniZanika <yuyunisyerin@gmail.com>
2022-03-11 16:09:20 +0700

    Percobaan Blok Dan Komentar

---
+++ src/main/kotlin/3_Blok_dan_Komentar.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Hello, selamat datang di halaman blok dan komentar!!")
+
+    // komentar 1 baris
+    /*
+    1 baris atau lebih
+     */
+    /**
+     * kotlin Doc --> baris ini untuk mendokementasikan kode program
+     */
+}
