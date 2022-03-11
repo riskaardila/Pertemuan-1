d40f30c6746088c4891a3ef98b0c8168514b3c40
RPL-YuniZanika <yuyunisyerin@gmail.com>
2022-03-11 16:50:20 +0700

    Percobaan Underscore Dalam Literal

---
+++ src/main/kotlin/8_Underscore_Dalam_Literal.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Penggunaan underscore dalam literal dapat memudahkan untuk menulis dan membaca kode program tersebut.
+    val oneMillion = 1_000_000
+    val creaditCardNumber = 1234_5678_9012_3456L
+    val socialSecurityNumber = 999_99_9999L
+    val hexBytes = 0xFF_EC_DE_5E
+    val bytes = 0b11010010_01101001_10010100_10010010
+
+    //mencetak dari kode program
+}
