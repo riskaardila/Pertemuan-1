3dc776500dfbc2064ff2f877625a6907a0386787
RPL-YuniZanika <yuyunisyerin@gmail.com>
2022-03-11 16:15:55 +0700

    Percobaan Variabel

--- src/main/kotlin/4_Variabel.kt
+++ src/main/kotlin/4_Variabel.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
     //  ini sebuah kode program variabel
+    // contoh Penggunaan Dinamis
     var dinamisVar = 2
     println("Contoh dinamis variabel = " + dinamisVar)
+
+    //contoh penggunaan Statis
+    var staticVar: Char = 'A'
+    println("Contoh static variabel")
 }
