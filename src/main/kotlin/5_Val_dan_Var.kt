6e6501e53438202cb44e57c8263a7f9781b19834
RPL-YuniZanika <yuyunisyerin@gmail.com>
2022-03-11 16:23:15 +0700

    Percobaan Val dan Var Variabel

---
+++ src/main/kotlin/5_Val_dan_Var.kt
@@ -0,0 +1,12 @@
+fun main() {
+    // contoh penggunaan Var Variabel
+    var varVariabel: Int
+    varVariabel = 56 //Menetapkan nilai pertama ke varVariabel
+    println("Pemberian nialai pertama pada varVAriable = " + varVariabel)
+    varVariabel = 78 //Menetapkan nilai kedua ke varVariabel
+    println("Pemberian nilai kedua pada varVariabel = " + varVariabel)
+
+    // Contoh penggunaan Val Variabel
+    val phi = 3.14
+    phi = 2.5 // akan terjadi error, karena nilai val tidak bisa diubah.
+}
