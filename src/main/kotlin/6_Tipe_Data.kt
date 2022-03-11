e4e0951069a64f6692db6b75be21e00b0bb2e61e
RPL-YuniZanika <yuyunisyerin@gmail.com>
2022-03-11 16:33:44 +0700

    Percobaan Tipe Data

---
+++ src/main/kotlin/6_Tipe_Data.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    val minByte: Byte = -128
+    val maxByte: Byte = 127 // untuk 8bit
+    val minShort: Short = -32768
+    val maxShort: Short = 3276 // untuk 16bit
+    val minInt: Int = -2147483648
+    val maxInt: Int = 214748364
+    val minLong: Long = -9223372036854775807
+    val maxLong: Long = 9223372036854775807
+
+    //untuk mencetak
+    println("minByte: " + minByte)
+    println("maxByte" + maxByte)
+    println("minShort" + minShort)
+    println("maxShort" + maxShort)
+    println("minInt" + minInt)
+    println("maxInt" + maxInt)
+    println("minLong" + minLong)
+    println("maxLong" + maxLong)
+}
