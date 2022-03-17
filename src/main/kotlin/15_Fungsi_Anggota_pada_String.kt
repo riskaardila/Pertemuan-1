b5868f00b98d279e4dd1c91c5a8ef5250b6461ec
Riska Ardila <riskaardila35@gmail.com>
2022-03-17 20:40:33 +0700

    Percobaan Fungsi Anggota pada String

---
+++ src/main/kotlin/15_Fungsi_Anggota_pada_String.kt
@@ -0,0 +1,19 @@
+fun main() {
+   val s = "RiskaArdila"
+   println(s.startsWith("Ris"))
+    println(s.endsWith("dila"))
+    println(s.contains("kaAr"))
+    println(s.contains("lol"))
+
+    // replace()
+    var strJava = "Java yang terbaik"
+    strJava = strJava.replace("Java","Kotlin")
+    println(s)
+    println("$strJava adalah ${strJava.length} Karakter Long")
+
+    // Substring()
+    println("Saya tidak akan membuang semua internet ini.".substring(2, 7))
+
+    // compareTo()
+    println("alpha".compareTo("bravo"))
+    }
